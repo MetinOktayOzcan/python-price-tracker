8992a2242c120ecde4ba397f49c1138538bf69f8
MetinOktayOzcan <metinoktayozcan@gmail.com>
2025-12-26 01:17:22 +0300

    Veritabanı mimari yenileme ve urun ekleme düzeltmesi

--- database.py
+++ database.py
@@ -1,35 +1,67 @@
 import sqlite3
+import datetime
 
 conn=sqlite3.connect('fiyatTakip.db')
-
 c =conn.cursor()
 
-def veritabaniOlustur():    
+def veritabaniOlustur():
+    c.execute("PRAGMA foreign_keys = ON;") 
     c.executescript("""
     CREATE TABLE IF NOT EXISTS urunBilgi(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            urunAdı text,
-            urunSitesi text,
-            urunUrl text
+            urunAdi TEXT UNIQUE,
+            urunEklemeTarihi DATETIME   
+            );                
+
+    CREATE TABLE IF NOT EXISTS urunAdresi(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            product_id INTEGER,
+            urunSiteIsmi TEXT,
+            urunUrl TEXT UNIQUE,
+            urunAktif BOOLEAN DEFAULT 1,    
+            urunGuncelFiyat REAL,
+            FOREIGN KEY(product_id) REFERENCES urunBilgi(id) ON DELETE CASCADE
             );
             
     CREATE TABLE IF NOT EXISTS urunFiyat(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            urun_id INTEGER,
+            link_id INTEGER,
             urunFiyat REAL,
-            urunTarih TEXT,
-            FOREIGN KEY(urun_id) REFERENCES urunBilgi(id)
+            urunTarih DATETIME,
+            FOREIGN KEY(link_id) REFERENCES urunAdresi(id) ON DELETE CASCADE
             );
     """)
     conn.commit()
 
-def veritabaniKayit(urunAdi,urunSitesi,urunUrl):
-    c.execute("INSERT INTO urunBilgi (urunAdı,urunSitesi,urunUrl) VALUES (?,?,?)", (urunAdi, urunSitesi, urunUrl))
-    conn.commit()
+veritabaniOlustur()
 
-#veritabaniKayit("amazon","amazon","amazon")
-c.execute("SELECT * FROM urunBilgi")
-print(c.fetchall())
+def urunEkle(urunAdi):
+    c.execute("SELECT id FROM urunBilgi WHERE urunAdi=?",(urunAdi,))
+    veri = c.fetchone()
+
+    if veri is not None:
+        return "Bu ismiyle Kayıtlı Ürün Bulunuyor"
+    else:
+        zaman = datetime.datetime.now()
+        c.execute("INSERT INTO urunBilgi (urunAdi,urunEklemeTarihi) VALUES (?,?)", (urunAdi,zaman))
+        conn.commit()
+
+
+# #Veritabanı testi için eklenen kodlar
+# urunEkle("test")
+
+# c.execute("SELECT * FROM urunBilgi")
+# bilgiler = c.fetchall()
+# for bilgi in bilgiler:
+#     print(bilgi)
+
+        
+
+
+
+
+
+
 
 
 
